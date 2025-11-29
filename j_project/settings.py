5901fff357baad30d063d67ce05be7f69cd47e24
mastermav <marvin.rigler@gmail.com>
2025-11-29 08:42:50 +0100

    Adjust ALLOWED_HOSTS for Render

--- j_project/settings.py
+++ j_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-a$#9_6=g(04l$6+)ea_#h$1m=p1%oda!+p8^-x$0s7u3f-v@(w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".onrender.com"]
 
 
 # Application definition
@@ -122,10 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-import os
-
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
